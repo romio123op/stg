633d2bba3985a6849cf5ab3aa06f2f54bcbfadf1
romio123op <tuananh.vu100796@gmail.com>
2022-05-24 15:39:12 +0700

    new

---
+++ sethosts.sh
@@ -0,0 +1,8 @@
+192.168.10.200  kmaster1    kmaster1.lan
+192.168.10.201  kmaster2    kmaster2.lan
+192.168.10.202  kmaster3    kmaster3.lan
+192.168.10.210  loadbalancer k8s.loabalancer.lan loadbalancer.lan  k8sloadbalancer k8sloadbalancer.lan
+192.168.10.111  repo.fabi.lan
+
+192.168.10.221  kworker1
+192.168.10.222  kworker2
